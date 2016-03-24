c31e09b85bea6655f33e1150d597368e6cc05745
EugeneKi <aiqcms@gmail.com>
2016-03-24 10:56:37 +0300

    Add news viewsets

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from news.models import WpPosts
+from news.serializers import NewsSerializer
+
+
+class NewsViewSet(viewsets.ModelViewSet):
+    queryset = WpPosts.objects.filter(post_type='post', post_status='publish').order_by(
+        '-post_date')
+    serializer_class = NewsSerializer
