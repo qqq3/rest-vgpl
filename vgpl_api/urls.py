84ae394cba5a13dc2208ee02cf119b47d42a8d97
EugeneKi <aiqcms@gmail.com>
2016-03-24 14:46:43 +0300

    Add redirect url

--- vgpl_api/urls.py
+++ vgpl_api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url('$^', RedirectView.as_view(url='http://www.vysokskylycey.by/')),
     url(r'^admin/', admin.site.urls),
     url(r'^news/', include('news.urls')),
 ]
