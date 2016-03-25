c4c44165d3287777b060118c0fe19e1e616a8442
EugeneKi <aiqcms@gmail.com>
2016-03-25 10:05:26 +0300

    Replace viewsset to APIView in news app

--- news/urls.py
+++ news/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import url
 
-from news.views import NewsViewSet
+from news import views
 
 app_name = 'news'
 
-news_list = NewsViewSet.as_view({
-    'get': 'list',
-})
-news_detail = NewsViewSet.as_view({
-    'get': 'retrieve',
-})
-
 urlpatterns = [
-    url(r'^$', news_list, name='news_list'),
-    url(r'^(?P<pk>[0-9]+)/$', news_detail, name='news_detail'),
+    url(r'^$', views.NewsList.as_view(), name='news_list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.NewsDetail.as_view(), name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,10 +1,30 @@
-from rest_framework import viewsets
-
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from news.models import WpPosts
 from news.serializers import NewsSerializer
 
 
-class NewsViewSet(viewsets.ModelViewSet):
-    queryset = WpPosts.objects.filter(post_type='post', post_status='publish').order_by(
-        '-post_date')
-    serializer_class = NewsSerializer
+class NewsList(APIView):
+    @staticmethod
+    def get(request):
+        all_news = WpPosts.objects.filter(post_type='post', post_status='publish').order_by(
+            '-post_date')
+        serializer_class = NewsSerializer(all_news, many=True)
+
+        return Response({'result': serializer_class.data})
+
+
+class NewsDetail(APIView):
+    @staticmethod
+    def get_object(pk):
+        try:
+            return WpPosts.objects.get(pk=pk)
+        except WpPosts.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        news = self.get_object(pk)
+        serializer_class = NewsSerializer(news)
+
+        return Response(serializer_class.data)
