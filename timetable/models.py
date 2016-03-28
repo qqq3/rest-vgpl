c6e9bade98f10fded405f51f3dd4c31fa6c0ac1c
EugeneKi <aiqcms@gmail.com>
2016-03-28 15:14:09 +0300

    Reorgineze timetable models

--- timetable/models.py
+++ timetable/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 
+class Groups(models.Model):
+    group_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.group_name
+
+
 class Timetable(models.Model):
     timetable_name = models.CharField(max_length=50)
-    week_day = models.CharField(max_length=30, choices=(('Monday', 'monday'), ('Tuesday', 'tuesday')))
+    week_day = models.CharField(max_length=30,
+                                choices=(('Monday', 'monday'), ('Tuesday', 'tuesday')))
+    group = models.ForeignKey(Groups)
 
     def __str__(self):
         return self.timetable_name
@@ -12,11 +21,3 @@ class Timetable(models.Model):
 class Lesson(models.Model):
     lesson = models.CharField(max_length=50)
     timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE)
-
-
-class Groups(models.Model):
-    group_name = models.CharField(max_length=50)
-    timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.group_name
