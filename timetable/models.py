9ddd1d14578e86d6bafde83df15440959513b339
EugeneKi <aiqcms@gmail.com>
2016-03-28 11:29:59 +0300

    Change group name field type, add __str__ method

--- timetable/models.py
+++ timetable/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 
 class Groups(models.Model):
-    group_name = models.IntegerField(max_length=25)
+    group_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.group_name
 
