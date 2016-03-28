1ea9f87f2116a74d2bf93d682b0661bff6e0e62d
EugeneKi <aiqcms@gmail.com>
2016-03-28 12:14:31 +0300

    Edit timetable models - add Timtable and Lesson classes

--- timetable/models.py
+++ timetable/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 
+class Timetable(models.Model):
+    timetable_name = models.CharField(max_length=50)
+    week_day = models.CharField(choices=(('Monday', 'monday'), ('Tuesday', 'tuesday')))
+
+    def __str__(self):
+        return self.timetable_name
+
+
+class Lesson(models.Model):
+    lesson = models.CharField(max_length=50)
+    timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE)
+
+
 class Groups(models.Model):
     group_name = models.CharField(max_length=50)
+    timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.group_name
-
