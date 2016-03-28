af2566a13d2b12f3db005823f8fbbd66454488af
EugeneKi <aiqcms@gmail.com>
2016-03-28 15:49:06 +0300

    Add elements on Timetable admin

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Groups
+from .models import Groups, Lesson, Timetable
 
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class TimetableAdmin(admin.ModelAdmin):
+    list_display = ('timetable_name', 'group', )
+    inlines = [LessonInline]
+
+
+admin.site.register(Timetable, TimetableAdmin)
 admin.site.register(Groups)
