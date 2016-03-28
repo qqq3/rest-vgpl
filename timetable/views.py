267dc994d0f065a6158cb860c415e4da75963af0
EugeneKi <aiqcms@gmail.com>
2016-03-28 11:31:18 +0300

    Add groups list class into timetable views

--- timetable/views.py
+++ timetable/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from timetable.serializers import GroupSerializer
+from timetable.models import Groups
 
-# Create your views here.
+
+class GroupsList(APIView):
+    def get(self, request):
+        all_groups = Groups.objects.all()
+        serializer = GroupSerializer(all_groups, many=True)
+
+        return Response(serializer.data)
