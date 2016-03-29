33ee385f015bd150fbb6c7d9e0575ca95eb85d74
EugeneKi <aiqcms@gmail.com>
2016-03-29 17:46:30 +0300

    Add group filter to timetable views

--- timetable/views.py
+++ timetable/views.py
@@ -1,12 +1,17 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.filters import DjangoFilterBackend
+from rest_framework import generics
 from timetable.serializers import TimetableSerializer
 from timetable.models import Timetable
 
 
-class TimetableList(APIView):
-    def get(self, request):
-        all_groups = Timetable.objects.all()
-        serializer = TimetableSerializer(all_groups, many=True)
+class TimetableList(generics.ListAPIView):
+    serializer_class = TimetableSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('week_day',)
 
-        return Response({'result': serializer.data})
+    def get_queryset(self):
+        queryset = Timetable.objects.all()
+        group = self.request.query_params.get('group', None)
+        if group is not None:
+            queryset = queryset.filter(group__group_name=group)
+        return queryset
