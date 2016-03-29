2061570c912b3fd336b0a57ea3d6049b4ff43fb9
EugeneKi <aiqcms@gmail.com>
2016-03-29 15:53:49 +0300

    Add timetable serializer

--- timetable/serializers.py
+++ timetable/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from timetable.models import Groups
+from timetable.models import Groups, Timetable
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Groups
         fields = ('group_name',)
+
+
+class TimetableSerializer(serializers.ModelSerializer):
+    group = GroupSerializer()
+    lessons = serializers.SlugRelatedField(many=True, read_only=True,
+                                           slug_field='lesson')
+
+    class Meta:
+        model = Timetable
+        fields = ('timetable_name', 'group', 'week_day', 'lessons')
