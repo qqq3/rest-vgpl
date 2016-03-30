65cdaaaf002fb8892f5d38ceb32e949c6a30d093
EugeneKi <aiqcms@gmail.com>
2016-03-30 12:08:37 +0300

    Add fieldsets into timetable admin

--- timetable/admin.py
+++ timetable/admin.py
@@ -2,14 +2,23 @@ from django.contrib import admin
 from .models import Groups, Lesson, Timetable
 
 
-class LessonInline(admin.StackedInline):
+class LessonInline(admin.TabularInline):
     model = Lesson
     extra = 5
 
 
 class TimetableAdmin(admin.ModelAdmin):
-    list_display = ('timetable_name', 'group', )
+    list_display = ('group', 'week_day')
+    fieldsets = (
+        ('Main information', {
+            'fields': ('group', 'week_day'),
+        }),
+        ('Notes', {
+            'fields': ('note', ), 'classes': ('collapse',),
+        }),
+    )
     inlines = [LessonInline]
+    list_filter = ['group', 'week_day', ]
 
 
 admin.site.register(Timetable, TimetableAdmin)
